fec1cb8d52772305dd0e4044ff143ed207f79cef
Zemorath <trentward10@gmail.com>
2023-09-28 22:53:21 -0500

    Routes Lab Compelete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div><h1>Direcors Page</h1>{directors.map((director) => (
+    <div key={director.name}>
+      {director.name}
+      <ul>
+        {director.movies.map((movie) => (
+          <li>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))}</div>;
 }
 
 export default Directors;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import NavBar from "./components/NavBar";
+import Directors from "./components/Directors";
+import Actors from "./components/Actors";
 
 ReactDOM.render(
   <Router>
